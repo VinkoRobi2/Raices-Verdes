6488153d19c9f7aaba86bda8f9efa170537e0d3e
VinkoRobi <Vinkolino178@gmail.com>
2024-12-18 14:11:33 -0500

    Bryan vale verga

--- RV/src/Footer.jsx
+++ RV/src/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import img1 from "./assets/1.png"
 import img3 from "./assets/3.png"
 import img2 from "./assets/2.png"
+import img4 from "./assets/4.png"
 import './Footer.css';
 
 const SocialIcon = ({ href, src, alt }) => (
@@ -23,6 +24,7 @@ function Footer() {
           <SocialIcon href="https://instagram.com" src={img2} alt="Instagram" />
           <SocialIcon href="https://facebook.com" src={img1} alt="Facebook" />
           <SocialIcon href="https://tiktok.com" src={img3} alt="TikTok" />
+          <SocialIcon href="https://tiktok.com" src={img4} alt="Whatsapp" />
         </div>
       </div>
     </footer>
