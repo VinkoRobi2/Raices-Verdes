0b8bea5e4054b04ed07a2ed95e40dc98d6b84530
VinkoRobi2 <vinkolino1@gmail.com>
2025-07-25 09:25:02 -0500

    Img

--- RV/src/QuienesSomos.jsx
+++ RV/src/QuienesSomos.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import './QuienesSomos.css';
+import q from "../src/assets/q.png"
 
 function QuienesSomos() {
   const sectionRef = useRef(null);
@@ -37,16 +38,18 @@ function QuienesSomos() {
       <h1></h1>
       <h1></h1>
       <h1></h1>
-        <section className="quienes-somos" ref={sectionRef}>
-      <div className="quienes-somos-content">
-        <h2>Quiénes Somos</h2>
-        <p>En Raíces Verdes somos apasionados por transformar espacios con césped natural de la más alta calidad. Nos dedicamos a la producción, comercialización y colocación de césped, respaldados por un equipo de profesionales especializados en césped y espacios verdes. Nos comprometemos a ofrecerte la mejor calidad, atención personalizada y un excelente servicio.</p>
-        <p>Contamos con diversas variedades de césped que se adaptan a cualquier necesidad: áreas de esparcimiento, campos deportivos y espacios públicos.</p>
-        <p>Gracias a nuestra tecnología de punta y personal altamente calificado, garantizamos productos de calidad superior.</p>
-      </div>
-    </section>
+      <section className="quienes-somos" ref={sectionRef}>
+        <div className="quienes-somos-content">
+          <h2>Quiénes Somos?</h2>
+          <p>En Raíces Verdes somos apasionados por transformar espacios con césped natural de la más alta calidad. Nos dedicamos a la producción, comercialización y colocación de césped, respaldados por un equipo de profesionales especializados en césped y espacios verdes. Nos comprometemos a ofrecerte la mejor calidad, atención personalizada y un excelente servicio.</p>
+          <p>Contamos con diversas variedades de césped que se adaptan a cualquier necesidad: áreas de esparcimiento, campos deportivos y espacios públicos.</p>
+          <p>Gracias a nuestra tecnología de punta y personal altamente calificado, garantizamos productos de calidad superior.</p>
+        </div>
+        <div className="quienes-somos-image">
+          <img src={q} alt="Césped natural" />
+        </div>
+      </section>
     </div>
-  
   );
 }
 
